f6b15a68f1837606b0bf77041442e911454f0a3a
Akinkunmi Okunola <Akinkunmi.okunola@wemabank.com>
2023-04-16 19:50:24 +0100

    Add project files.

---
+++ ProductMicroservice/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductMicroservice.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public decimal Cost { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+    }
+}
